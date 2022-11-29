ce79a436e16419c4248a8052922216b31280440a
Ricardo Yegros <ricardo_yegros@hotmail.com>
2022-11-29 14:02:27 -0300

    redux installed and config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store/";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
---
+++ src/redux/reducers/example.js
@@ -0,0 +1,19 @@
+// import { ADD_FAVORITE } from from "../actions/example.js
+
+// const initialState = {
+//     favorite: [],
+//     message: {},
+//     products: [],
+// };
+
+// export default function reviewsDashReducer (state = initialState, action) {
+//     switch (action.type) {
+//         case ADD_FAVORITE:
+//             return {
+//                 ...state,
+//                 message: action.payload
+//             };
+//             default:
+//                 return state;
+//         };
+//     };
