e2c9b441ddd11e7ac8a66b4542690033efa4600d
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-11-29 14:33:36 -0300

    added default title size and valid size options

--- src/components/title/index.jsx
+++ src/components/title/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 export const Title = ({size, children})=> {
-    return <TitleTag as={size}>{children}</TitleTag>
+    const sizes = ["h1", "h2", "h3", "h4"];
+    
+    if (sizes.includes(size)){
+        return <TitleTag as={size}>{children}</TitleTag>
+    } else {
+        return <TitleTag as="h1">{children}</TitleTag>
+    }
+
 };
 
 const TitleTag = styled.h2``
