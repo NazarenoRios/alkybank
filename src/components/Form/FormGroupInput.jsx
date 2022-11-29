e4448317e32c034187bc36d6ca0c2404ce699112
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-29 16:37:40 -0300

    feat: add input styles

--- src/components/Form/FormGroupInput.jsx
+++ src/components/Form/FormGroupInput.jsx
@@ -2,17 +2,18 @@ import { ErrorMessage, Field } from "formik"
 
 export default function FormGroupInput({ type, label, placeholder, minLength, maxLength }) {
   return (
-    <div>
-      <label htmlFor={type}>{label}</label>
-      <div>
-        <Field
-          name={type}
-          id={type}
-          placeholder={placeholder}
-          minLength={minLength}
-          maxLength={maxLength}
-        ></Field>
-      </div>
+    <div className="flex flex-col items-start w-full text-[#FF7085]">
+      <label className="text-[#ffffff]" htmlFor={type}>
+        {label}
+      </label>
+      <Field
+        className="w-full rounded-md p-2 px-3 text-[#8e9297] focus:outline outline-1 outline-[#0092BE] border-none"
+        name={type}
+        id={type}
+        placeholder={placeholder}
+        minLength={minLength}
+        maxLength={maxLength}
+      ></Field>
       <ErrorMessage name={type} />
     </div>
   )
