fe58e268d7d82c9bd2955521d5ef24bd2f518527
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-30 14:27:16 -0300

    feat: add sign up route

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
+import React from "react"
+import { Routes, Route, Navigate } from "react-router-dom"
+import "./App.css"
 
 //Pages
-import Homepage from "./pages/Homepage";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
+import Homepage from "./pages/Homepage"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
+import SignUp from "./pages/SignUp"
 
 function App() {
-  
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="/*" element={<Navigate to={"404"} />} />
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
