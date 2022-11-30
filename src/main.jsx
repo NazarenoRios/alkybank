d6129ba790a62e9e1a074a82716fb0de299ba3b7
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-30 19:49:50 -0300

    feat: add firebase configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
+import App from "./App"
+import "./index.css"
+import "../firebase/firebase"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,4 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </Router>
   </React.StrictMode>
-);
+)
