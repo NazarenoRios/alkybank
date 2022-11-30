25389bd47db9e689af299c3945e1b3ae62478d32
jcaicedo <jose.caicedo@outlook.com>
2022-11-30 09:39:59 -0500

    feat:colors and typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,28 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        'primary': '#C8EE44',
+        'secondary': '#29A073',
+        'error': '#E5363D',
+        'success': '#19D076',
+        'text1': '#1B212D',
+        'text2': '#929EAE',
+        'text3': '#ff77e9',
+        'dark1': '#201E34',
+        'dark2': '#282541',
+        'dark_bg': '#1C1A2E',
+        'dark_shade': '#1E1C30',
+        'key_black': '#363A3F',
+        'gray1': '#FAFAFA',
+        'gray2': '#F8F8F8',
+        'gray3': '#F5F5F5',
+      },
+      fontFamily: {
+        KumbhSans:['Kumbh Sans', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
