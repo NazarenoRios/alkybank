ce3670076c6427ec70e1e3c30f750d844ff3d7f7
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-01 15:06:59 -0300

    feat: add getDate

--- src/components/Form/useSignUpForm.js
+++ src/components/Form/useSignUpForm.js
@@ -8,9 +8,26 @@ export const useSignUpForm = () => {
     createUser(values)
       .then(response => {
         console.log(response)
+        const userId = response.id
         navigate("/login")
       })
       .catch(error => console.log(error))
+  }
+
+  const getDate = () => {
+    const rawCurrentDate = new Date().toLocaleDateString("es-AR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    })
+    const rawCurrentDay = new Date().toLocaleDateString("es-AR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    })
+    const formatCurrentDate = rawCurrentDate.split("/").reverse().join("/").replaceAll("/", "-")
+    const formatCurrentDay = rawCurrentDay.split(",")[1]
+    return `${formatCurrentDate} ${formatCurrentDay}`
   }
 
   return { handleSubmit }
