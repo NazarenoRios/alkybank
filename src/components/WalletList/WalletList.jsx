7631e5f22edfd67b15154610697c2b4a88b73dc5
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-01 16:03:14 -0300

    feat: add wallet list

---
+++ src/components/WalletList/WalletList.jsx
@@ -0,0 +1,18 @@
+import Button from "../Button"
+import Wallet from "../Wallet/Wallet"
+
+export default function WalletList() {
+  return (
+    <section className="w-full max-w-[354px] flex flex-col justify-center gap-5  m-3 ">
+      <header className="flex justify-between items-center">
+        <h2 className="font-semibold text-[18px]">Wallet</h2>
+        <Button variant="primary" text="Create new wallet" />
+      </header>
+      <ul className="flex flex-col justify-center">
+        <li>
+          <Wallet />
+        </li>
+      </ul>
+    </section>
+  )
+}
