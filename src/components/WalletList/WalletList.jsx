f70a68c31c7a27e01fe7576d25344cea384d43f0
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-01 16:29:31 -0300

    feat: add create wallet modal

--- src/components/WalletList/WalletList.jsx
+++ src/components/WalletList/WalletList.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react"
 import Button from "../Button"
 import Wallet from "../Wallet/Wallet"
+import CreateWalletModal from "./CreateWalletModal"
 
 export default function WalletList() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <section className="w-full max-w-[354px] flex flex-col justify-center gap-5 px-2 box-border">
       <header className="flex justify-between items-center">
         <h2 className="font-semibold text-[18px]">Wallet</h2>
-        <Button variant="primary" text="Create new wallet" />
+        <Button variant="primary" text="Create new wallet" action={() => setShowModal(true)} />
       </header>
       <ul className="flex flex-col justify-center">
         <li>
           <Wallet />
         </li>
       </ul>
+      {showModal ? <CreateWalletModal showModal={showModal} setShowModal={setShowModal} /> : null}
     </section>
   )
 }
