7fd2b577d9b954283da1d290ae9b02c8bdeabfda
thadek <gabipamic@gmail.com>
2022-12-03 09:35:28 -0300

    fixes

--- src/components/TopupMoneyForm/TopupMoneyForm.jsx
+++ src/components/TopupMoneyForm/TopupMoneyForm.jsx
@@ -3,101 +3,96 @@ import { useForm } from "react-hook-form";
 import Button from "../Button";
 import AlkemyLogo from "../../assets/alkemy-logo-white.png";
 import { useDispatch } from "react-redux";
-import {topupAction} from "../../redux/actions/topupAction";
+import { topupAction } from "../../redux/actions/topupAction";
 
 export default function TopupMoneyForm() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   const onSubmit = ({ amount, concept }) => {
-    console.log(amount, concept)   
+    console.log(amount, concept)
     dispatch(topupAction(amount, concept));
-    }
+  }
   return (
     <section className="h-full gradient-formmd:h-screen ">
       <div className="container py-12 px-6 h-full">
         <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-          <div className="">
-            <div className="block bg-dark_bg rounded-lg shadow-md shadow-white">
-              <div className="">
-                <div className="">
-                  <div className="md:p-12 md:mx-6">
-                    <div className="text-center">
-                      <img
-                        className="mx-auto w-48 mb-4"
-                        src={AlkemyLogo}
-                        alt="logo"
-                      />
-                      <h4 className="text-xl mt-1 mb-12 pb-1 text-white">
-                        Send money to whoever you want
-                      </h4>
-                    </div>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                      <div className="mb-4 text-white">
-                        <label>Amount</label>
-                        <input
-                          type="number"
-                          className={`form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none ${
-                            errors.amount &&
-                            `focus:border-red-600 focus:ring-red-600 border-red-600`
-                          }`}
-                          placeholder="Amount"
-                          {...register("amount", {
-                            required: "Amount is required",
-                            pattern: {
-                                 value:
-                                 /^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$/gm,
-                              message: "Please enter a valid amount",
-                            },
-                          })}
-                        />
-                        {errors.amount && (
-                          <span className="text-red-600 text-sm">
-                            {errors.amount.message}
-                          </span>
-                        )}
-                      </div>
-                      <div className="mb-4 text-white">
-                        <label>Concept</label>
-                        <input
-                          type="text"
-                          className={`form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none ${
-                            errors.concept &&
-                            `focus:border-red-600 focus:ring-red-600 border-red-600`
-                          }`}
-                          placeholder="concept"
-                          {...register("concept", {
-                            required: "Concept is required",
-                            pattern: {
-                              //   value:
-                              //     /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                              message: "Please enter a concept",
-                            },
-                          })}
-                        />
-                        {errors.concept && (
-                          <span className="text-red-600 text-sm">
-                            {errors.concept.message}
-                          </span>
-                        )}
-                      </div>
-                      <div className="text-center pt-1 mb-12 pb-1 w-full ">
-                        <Button
-                          className="max-w-[400px] "
-                          variant="primary"
-                          type="submit"
-                          onSubmit={onSubmit}
-                        >
-                          Topup Money
-                        </Button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
+
+          <div className="block bg-dark_bg rounded-lg shadow-md shadow-white">
+
+
+            <div className="md:p-12 md:mx-6">
+              <div className="text-center">
+                <img
+                  className="mx-auto w-48 mb-4"
+                  src={AlkemyLogo}
+                  alt="logo"
+                />
+                <h4 className="text-xl mt-1 mb-12 pb-1 text-white">
+                  Send money to whoever you want
+                </h4>
               </div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4 text-white">
+                  <label>Amount</label>
+                  <input
+                    type="number"
+                    className={`form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none ${errors.amount &&
+                      `focus:border-red-600 focus:ring-red-600 border-red-600`
+                      }`}
+                    placeholder="Amount"
+                    {...register("amount", {
+                      required: "Amount is required",
+                      pattern: {
+                        value:
+                          /^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$/gm,
+                        message: "Please enter a valid amount",
+                      },
+                    })}
+                  />
+                  {errors.amount && (
+                    <span className="text-red-600 text-sm">
+                      {errors.amount.message}
+                    </span>
+                  )}
+                </div>
+                <div className="mb-4 text-white">
+                  <label>Concept</label>
+                  <input
+                    type="text"
+                    className={`form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none ${errors.concept &&
+                      `focus:border-red-600 focus:ring-red-600 border-red-600`
+                      }`}
+                    placeholder="concept"
+                    {...register("concept", {
+                      required: "Concept is required",
+                      pattern: {
+                        //   value:
+                        //     /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "Please enter a concept",
+                      },
+                    })}
+                  />
+                  {errors.concept && (
+                    <span className="text-red-600 text-sm">
+                      {errors.concept.message}
+                    </span>
+                  )}
+                </div>
+                <div className="text-center pt-1 mb-12 pb-1 w-full ">
+                  <Button
+                    className="max-w-[400px] "
+                    variant="primary"
+                    type="submit"
+                    onSubmit={onSubmit}
+                  >
+                    Topup Money
+                  </Button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
