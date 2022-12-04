54c5f7a21e6dc056b1a3124636f9e76c47b680b8
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-04 09:58:18 -0300

    feat(wallet): add dynamic data

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -1,8 +1,11 @@
 import mastercardIcon from "../../assets/mastercard-icon.png"
 import chipIcon from "../../assets/chip-icon.png"
 import wifiIcon from "../../assets/wifi-icon.png"
+import { useSelector } from "react-redux"
 
 export default function Wallet() {
+  const walletState = useSelector(state => state.walletReducer)
+
   return (
     <div
       className="w-full max-w-[400px] h-[210px] rounded-[15px] relative p-[20px]"
@@ -18,9 +21,15 @@ export default function Wallet() {
         </div>
         <div className="flex flex-col ">
           <span className="text-white font-bold text-[22px] tracking-[0.1em]">
-            5495 7381 3759 2321
+            **** **** **** {walletState.id}
           </span>
-          <span className="text-[#929EAE]">09/25</span>
+          <span className="text-[#929EAE]">
+            {" "}
+            {new Date(walletState.createdAt).toLocaleDateString("es-AR", {
+              month: "2-digit",
+              day: "2-digit"
+            })}
+          </span>
         </div>
       </div>
       <img className="absolute right-5 bottom-5" src={mastercardIcon} alt="" />
