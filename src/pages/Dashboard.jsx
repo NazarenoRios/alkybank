16cd3d9b996b958f84f8449871dc46c025aa4973
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-03 21:06:18 -0300

    feat: add graph content

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import walletIconGreen from "../assets/wallet-icon-green.png"
 import walletIconDark from "../assets/wallet-icon-dark.png"
+import graph from "../assets/graph.png"
 
 export default function Dashboard() {
   return (
     <section className="ml-[290px] pt-[30px]">
-      <div>
+      <div className="flex flex-col gap-12 ">
         <h1 className="text-[25px] font-semibold">Dashboard</h1>
-        <div className="mt-[38.5px]">
+        <div>
           <div className="flex items-center gap-8">
             <div className="w-full max-w-[250px] flex gap-4 rounded-[10px] bg-key_black pl-[20px] pr-[20px] pt-[24px] pb-[24px]">
               <div className="w-fit h-fit p-3 rounded-[100px] bg-[#4E5257]">
@@ -27,6 +28,12 @@ export default function Dashboard() {
               </div>
             </div>
           </div>
+        </div>
+        <div className="relative">
+          <div className="blur-[2px]">
+            <img src={graph} alt="" />
+          </div>
+          <h3 className="absolute left-[15%] top-[50%] text-[40px]">Coming soon...</h3>
         </div>
       </div>
     </section>
