b3bb34ec887fe755e0d005601942c0dd6c2ee0f7
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-12-05 18:24:03 -0300

    added remove all localstorage information functionality to logout

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const Logout = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function logout () {
-        localStorage.removeItem("token")
-        // localStorage.removeItem("name")
-        // localStorage.removeItem("email")
-    }
+  function logout() {
+    localStorage.removeItem("last_name");
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("first_name");
+  }
 
-    useEffect(() => {
-        logout()
-        navigate("/")
-    });
+  useEffect(() => {
+    logout();
+    navigate("/");
+  });
 
-    return <div>Logout</div>
+  return <div>Logout</div>;
 };
 
-export default Logout
+export default Logout;
