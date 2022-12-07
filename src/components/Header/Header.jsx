f1c4a0cdee15872a3db28b7d10faf619f7e95caa
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-07 13:48:23 -0300

    fix: use correct icons on dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import avatar from "../../assets/avatar.png"
+import avatar from "../../assets/avatar.svg"
+import avatarDark from "../../assets/avatar-dark.svg"
 import arrowIcon from "../../assets/arrow-icon.png"
 import alkybankLogo from "../../assets/alkemy-logo.png"
 import moonIcon from "../../assets/moon-icon.svg"
@@ -10,28 +11,30 @@ export default function Header({ title }) {
   const { darkMode, setDarkMode } = useDarkModeContext()
 
   return (
-    <header className="w-full max-w-[1450px] h-[100px] flex items-center justify-between">
+    <header className="w-full h-[100px] flex items-center justify-between px-5 box-border">
       {/* <h1 className="text-[25px] font-semibold">{title}</h1>
        */}
       <a href="/" class="flex items-center pl-2.5 mb-5">
         <img src={alkybankLogo} class="mr-3 w-40 h-auto" alt="Alkemy bank Logo" />
       </a>
-      <div className="w-full max-w-[300px] h-[48px] flex items-center gap-4 rounded-[100px] bg-[#FAFAFA] px-5 box-border">
-        <img className="w-[36px]" src={avatar} alt="avatar" />
-        <div className="w-full flex justify-between">
-          <span>
-            {localStorage.first_name}
-            {localStorage.last_name === "null" ? "" : localStorage.last_name}
-          </span>
-          <img className="transform rotate-90" src={arrowIcon} alt="" />
-        </div>
+      <div className="flex gap-5 h-[40px] bg-[#FAFAFA] dark:bg-dark1 dark:text-white">
+        <button
+          className="w-[50px] flex justify-center items-center bg-dark2 dark:bg-[#f8f8f8] dark:text-white rounded-[10px] p-3 box-border"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          <img className="w-[20px]" src={darkMode ? sunIcon : moonIcon} alt="Toggle dark mode" />
+        </button>
+        <button className="w-full max-w-[300px] h-[40px] flex items-center gap-5 rounded-[100px]  px-5 box-border">
+          <img className="w-[20px]" src={darkMode ? avatar : avatarDark} alt="avatar" />
+          <div className="w-full flex justify-between gap-5">
+            <span>
+              {localStorage.first_name}{" "}
+              {localStorage.last_name === "null" ? "" : localStorage.last_name}
+            </span>
+            <img className="transform rotate-90" src={arrowIcon} alt="" />
+          </div>
+        </button>
       </div>
-      <button
-        className="bg-dark2 dark:bg-[#f8f8f8] dark:text-white rounded-[10px] p-3 box-border"
-        onClick={() => setDarkMode(!darkMode)}
-      >
-        <img className="w-[20px]" src={darkMode ? sunIcon : moonIcon} alt="Toggle dark mode" />
-      </button>
     </header>
   )
 }
