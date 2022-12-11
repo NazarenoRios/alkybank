a7b920605fe4bb4f070d0139ba7ebd221596404b
Ricardo Yegros <ricardo_yegros@hotmail.com>
2022-12-10 21:50:16 -0300

    adding new fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,54 @@ import { useAuth } from "./hooks/useAuth";
 import "./App.css";
 
 //Pages
-import Homepage from "./pages/Homepage"
-import Login from "./pages/Login"
-import NotFound from "./pages/NotFound"
-import SignUp from "./pages/SignUp"
-import Loading from "./pages/Loading"
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import SignUp from "./pages/SignUp";
+import Loading from "./pages/Loading";
 
-import TopupMoney from "./pages/TopupMoney"
-import Payments from "./pages/Payments"
-import Dashboard from "./pages/Dashboard"
+//Components
+import TopupMoney from "./pages/TopupMoney";
+import Payments from "./pages/Payments";
+import Dashboard from "./pages/Dashboard";
 import Balances from "./pages/Balances";
 import Movements from "./pages/Movements";
-import Logout from "./pages/Logout"
-import { DarkModeProvider } from "./contexts/DarkModeContext"
+import { DarkModeProvider } from "./contexts/DarkModeContext";
+import { Layout } from "./components/layout";
+
 function App() {
   const { isAuthenticated, isLoading } = useAuth();
-  return (
-    <>
-    <DarkModeProvider>
-    <ToastContainer/>
-      <Routes>
-        {isAuthenticated ? <Route path="/" element={<Dashboard />} /> : <Route path="/" element={<Homepage />} />}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="/*" element={<Navigate to={"404"} />} />
-        <Route path="/loading" element={<Loading />} />
-        <Route path="/topup" element={<TopupMoney />} />
-        <Route path="/payments" element={<Payments />} />
-        <Route path="/movements" element={<Movements />} />
-        <Route path="/balance" element={<Balances />} />
-      </Routes>
-    </DarkModeProvider>
-    </>
-  )
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <DarkModeProvider>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Layout><Dashboard /></Layout> } />
+          <Route path="/login" element={<Navigate to="/" />} />
+          <Route path="/topup" element={<Layout><TopupMoney /></Layout>} />
+          <Route path="/payments" element={<Layout><Payments /></Layout>} />
+          <Route path="/movements" element={<Layout><Movements /> </Layout>} />
+          <Route path="/balance" element={<Layout><Balances /></Layout>} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </DarkModeProvider>
+    );
+  } else {
+    return (
+      <DarkModeProvider>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/*" element={<Homepage />} />
+        </Routes>
+      </DarkModeProvider>
+    );
+  }
 }
-export default App
+export default App;
--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,7 +2,7 @@ import  SkeletonCard  from '../utils/skeleton/SkeletonLogo'
 
 export default function Loading(){
   return (
-  <div className="flex h-screen justify-center items-center">
+  <div className="flex justify-center items-center w-[15rem] m-auto">
     <SkeletonCard />
   </div>
 
