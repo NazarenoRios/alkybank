079bc4a845f920f04ec969371739943a361c97d3
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-12-12 19:51:20 -0300

    moved dropdown component from header file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ import moonIcon from "../../assets/moon-icon.svg";
 import sunIcon from "../../assets/sun-icon.svg";
 import avatar from "../../assets/avatar.svg";
 import "flowbite";
-import { useAuth } from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
+import { Dropdown } from "../dropdown";
 
 export default function Header() {
   const { darkMode, setDarkMode } = useDarkModeContext();
@@ -70,82 +70,3 @@ export default function Header() {
   );
 }
 
-function Dropdown() {
-  const { signout } = useAuth();
-
-  return (
-    <>
-      <button
-        id="dropdownDividerButton"
-        data-dropdown-toggle="dropdownDivider"
-        className="md:hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        <img
-          className="w-[20px] h-auto transform"
-          src={arrowIcon}
-          alt="arrow icon"
-        />
-      </button>
-
-      <div
-        id="dropdownDivider"
-        className="hidden w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-dark2 dark:divide-gray-600"
-      >
-        <ul
-          className="py-1 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDividerButton"
-        >
-          <li>
-            <Link
-              to="/topup"
-              className="block py-2 px-4 hover:bg-primary dark:hover:bg-primary dark:hover:text-black"
-            >
-              Add funds
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/balance"
-              className="block py-2 px-4 hover:bg-primary dark:hover:bg-primary dark:hover:text-black"
-            >
-              Balance
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              className="block py-2 px-4 hover:bg-primary dark:hover:bg-primary dark:hover:text-black"
-            >
-              Expenses
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/movements"
-              className="block py-2 px-4 hover:bg-primary dark:hover:bg-primary dark:hover:text-black"
-            >
-              Last movements
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/payments"
-              className="block py-2 px-4 hover:bg-primary dark:hover:bg-primary dark:hover:text-black"
-            >
-              Send funds
-            </Link>
-          </li>
-        </ul>
-        <div className="py-1" onClick={signout}>
-          <Link
-            to="#"
-            className="block py-2 px-4 text-sm text-gray-700 hover:bg-primary dark:hover:bg-primary dark:text-gray-200 dark:hover:text-black"
-          >
-            Log out
-          </Link>
-        </div>
-      </div>
-    </>
-  );
-}
