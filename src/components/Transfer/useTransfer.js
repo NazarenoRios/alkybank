29f28c9f14c03d73132bf34dc9e10eda0ca4c5a3
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-12-13 12:42:31 -0300

    fix: fix amount value

--- src/components/Transfer/useTransfer.js
+++ src/components/Transfer/useTransfer.js
@@ -6,17 +6,19 @@ export const useTransfer = () => {
   const dispatch = useDispatch()
 
   const handleSubmit = async (values, walletState) => {
+    console.log(values)
     if (walletState < values.amount) return toast.error("You don't have enough money")
     getWalletByAccountId(values.accountId).then(error => {
       if (error.status === 500) return toast.error("The wallet id is not valid")
       transferToAccountId(values).then(error => {
+        console.log(error)
         if (error.status === 500) return toast.error("The wallet id is not valid")
         const token = sessionStorage.getItem("token")
         getWallet(token)
-          .then(wallets => dispatch({ type: "UPDATE_WALLET", payload: wallets[0] }))
-          .catch(error => {
-            return toast.error("An error has occurred. Try again later")
+          .then(wallets => {
+            dispatch({ type: "UPDATE_WALLET", payload: wallets[0] })
           })
+          .catch(() => toast.error("An error has occurred. Try again later"))
         return toast.success("Transfer successfully")
       })
     })
@@ -40,7 +42,7 @@ export const useTransfer = () => {
     const body = {
       type: values.type,
       concept: "string",
-      amount: values.amount
+      amount: Number(values.amount)
     }
     const response = await fetch(
       `http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com/accounts/${values.accountId}`,
